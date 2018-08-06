d400eed6c9acc9ac08db3a50ae2f878fc7481359
Kejia Wu <seraphwu@foxmail.com>
2018-08-06 21:53:24 +0800

    把guess重命名成为mid,这才能表示'中间index'的意思,以前的'guess'这个名字可能会被误以为是中间index的VALUE.

--- 1.1.binary_search.py
+++ 1.1.binary_search.py
@@ -5,30 +5,30 @@ def binary_search(obj, item):
     if obj[0] > item or obj[high] < item:
         return None
     while low <= high:
-        guess = int(((high - low) / 2) + low)
-        if obj[guess] == item:
-            # print('index %s is item %s' % (guess, item))
-            return guess
-        elif obj[guess] > item:
-            # print('%s > item' % guess)
-            high = guess - 1
-        elif obj[guess] < item:
-            # print('%s <= item' % guess)
-            low = guess + 1
+        mid = int(((high - low) / 2) + low)
+        if obj[mid] == item:
+            # print('index %s is item %s' % (mid, item))
+            return obj[mid]
+        elif obj[mid] > item:
+            # print('%s > item' % mid)
+            high = mid - 1
+        elif obj[mid] < item:
+            # print('%s <= item' % mid)
+            low = mid + 1
     return None
 
 
-# l = list(range(0, 100000))
+# l1 = list(range(0, 100000))
 
 # for i in range(10000):
-#     v = binary_search(l, i)
+#     v = binary_search(l1, i)
 #     if i != v:
 #         print('Error for i = %s and v = %s' % (i, v))
 #     else:
 #         print('%s -> %s' % (i, v))
 
 
-l = [1,4,6,7,8,22]
+l = [1, 4, 6, 7, 8, 22]
 # 不存在的数字
 print(binary_search(l, 5))
 # 范围之前的数字
@@ -42,4 +42,9 @@ print(binary_search(l, 1))
 # 结尾最后一位的数字
 print(binary_search(l, 22))
 
+
+l2 = [1, 6]
+print(binary_search(l2, 6))
+print(binary_search(l2, 1))
+print(binary_search(l2, 5))
 
