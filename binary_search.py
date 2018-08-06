d256d56f6d5c094a316448a910cb48b5a4e609ed
Kejia Wu <seraphwu@foxmail.com>
2018-08-06 21:20:24 +0800

    修复Binary search的BUG. 之前忽略了列表里的数字不是连续的,这样有些要查找的数字虽然大于第一个约束而且小于最后一个元素但它却不在列表里面,这时候以前的while True就会陷入死循环.

--- binary_search.py
+++ binary_search.py
@@ -4,20 +4,21 @@ def binary_search(obj, item):
     high = len(obj) - 1
     if obj[0] > item or obj[high] < item:
         return None
-    while True:
+    while low <= high:
         guess = int(((high - low) / 2) + low)
         if obj[guess] == item:
-            print('%s == item' % guess)
+            # print('index %s is item %s' % (guess, item))
             return guess
         elif obj[guess] > item:
-            print('%s > item' % guess)
+            # print('%s > item' % guess)
             high = guess - 1
         elif obj[guess] < item:
-            print('%s <= item' % guess)
+            # print('%s <= item' % guess)
             low = guess + 1
+    return None
 
 
-l = list(range(0, 100000))
+# l = list(range(0, 100000))
 
 # for i in range(10000):
 #     v = binary_search(l, i)
@@ -26,4 +27,19 @@ l = list(range(0, 100000))
 #     else:
 #         print('%s -> %s' % (i, v))
 
-print(binary_search(l, 100000-1))
+
+l = [1,4,6,7,8,22]
+# 不存在的数字
+print(binary_search(l, 5))
+# 范围之前的数字
+print(binary_search(l, -1))
+# 范围之后的数字
+print(binary_search(l, 30))
+# 中间的数字
+print(binary_search(l, 7))
+# 开始第一位的数字
+print(binary_search(l, 1))
+# 结尾最后一位的数字
+print(binary_search(l, 22))
+
+
