e61bc5a2987a5adb605a6f2a50c511cace1a7ee5
kwu <SERAPHWU@FOXMAIL.COM>
2019-04-25 22:16:04 +0800

    加了好些新的方法，同时试图理解变量赋值的含义。

--- Python/Data_Structure_and_Algorithm/linear_list.py
+++ Python/Data_Structure_and_Algorithm/linear_list.py
@@ -4,42 +4,42 @@ class LNode():
 		self.elem = elem
 		self.next = next_
 
-h = LNode(1)
-h.next = LNode(2)
-h.next.next = LNode(3)
-h.next.next.next = LNode(4)
+# h = LNode(1)
+# h.next = LNode(2)
+# h.next.next = LNode(3)
+# h.next.next.next = LNode(4)
 
 ## 定义head变量，它指向表里的第一个元素，这时候元素的顺序是 1，2，3，4
-head = h
+# head = h
 
 ## 1. 表首端的插入,完成以后这head的元素顺序是10，1，2，3，4
-n1 = LNode(10)
-n1.next = head
-head = n1
+# n1 = LNode(10)
+# n1.next = head
+# head = n1
 
 ## 2. 插入到2和3中间，完成以后head的元素顺序就是10,1,2,11,3,4
 ## 定义pre就是插入位置的前一个元素
-n2 = LNode(11)
-pre = head.next.next
-n2.next = pre.next
-pre.next = n2
+# n2 = LNode(11)
+# pre = head.next.next
+# n2.next = pre.next
+# pre.next = n2
 
 
 ## 3. 删除中间元素，比如删除2,那么pre就是2前面的元素
 ## 删除完毕以后，元素顺序是10,1,11,3,4
-pre = head.next
-pre.next = pre.next.next
-
+# pre = head.next
+# pre.next = pre.next.next
+#
 
 ## 4. 得到第4个元素,或者说下标=3的元素.下标从0开始.
-i = 4
-p = head
-while p is not None and i > 0:
-	i -= 1
-	p = p.next
-	if i == 0:
-		print("index = 3, value = ", p.elem)
-
+# i = 4
+# p = head
+# while p is not None and i > 0:
+# 	i -= 1
+# 	p = p.next
+# 	if i == 0:
+# 		print("index = 3, value = ", p.elem)
+#
 
 class LinkedListUnderflow(ValueError):
 	pass
@@ -52,9 +52,56 @@ class LList:
 	def is_empty(self):
 		return self._head is None
 
+	def prepend(self, elem):
+		self._head = LNode(elem, self._head)
 
+	def pop(self):
+		if self.is_empty():
+			raise LinkedListUnderflow("pop - list is empty")
+		q = self._head.elem
+		self._head = self._head.next
+		return q
 
-print("done")
+	def append(self, elem):
+		if self._head is None:
+			self._head = LNode(elem)
+			return
+		h = self._head
+		while h.next is not None:
+			h = h.next
+		h.next = LNode(elem)
+
+	def pop_last(self):
+		if self._head is None:
+			raise LinkedListUnderflow("pop_last -> head is None")
+		n, q = self._head, None
+		if n.next is None:
+			q = n.elem
+			self._head = None
+			return q
+		while n.next.next is not None:
+			n = n.next
+		q = n.next.elem
+		n.next = None
+		return q
+
+	def __str__(self):
+		s = ""
+		q = self._head
+		while q is not None:
+			s += "," + str(q.elem)
+			q = q.next
+		return s
+
+l = LList()
+for i in range(2):
+	l.prepend(i)
+
+# l.append(666)
+print("before pop_last:",l)
+print(l.pop_last())
+print(l.pop_last())
+print("after pop_last:",l)
 
 
 
