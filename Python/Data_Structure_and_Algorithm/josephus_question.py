114652cb71c0a5a0c17e8640a46e295b3aa832bd
kwu <SERAPHWU@FOXMAIL.COM>
2019-05-08 23:06:10 +0800

    Add Josephus question, second solution is "mark the item as 0".

--- Python/Data_Structure_and_Algorithm/josephus_question.py
+++ Python/Data_Structure_and_Algorithm/josephus_question.py
@@ -4,19 +4,34 @@
 # 结果：按照顺序输出每个退出人的编号。
 
 
-def josephus_mark_elem(n: int, k: int, m: int):
-	pass
-
-def josephus_delete_elem(n: int, k: int, m: int):
-	if n <= 0 or k <= 0 or m <= 0:
+def josephus_mark_elem(total_person: int, start_person: int, pop_number: int):
+	if total_person <= 0 or start_person <= 0 or pop_number <= 0:
 		raise ValueError("parameters cannot be 0")
-	input = list(range(1, n + 1))
-	crt_idx= k - 1
-	remain_item = n
-	print("Before, ", input)
-	for remain_item in range(n, 0, -1):
-		crt_idx = (crt_idx + m - 1) % remain_item
-		print("pop item is ", input.pop(crt_idx))
+	crt_index = start_person - 1
+	input = list(range(1, total_person + 1))
+	for remain_item in range(total_person, 0, -1):
+		valid_count = 0
+		inc = 0
+		while valid_count < pop_number:
+			# 千万小心，这个方法只标记0，不delete，所以下面始终是 % total_person
+			crt_index = (crt_index + inc) % total_person
+			if input[crt_index] == 0:
+				pass
+			else:
+				valid_count += 1
+			inc = 1
+		print("josephus_mark_elem - pop item value is ", input[crt_index])
+		input[crt_index] = 0
+	
+def josephus_delete_elem(total_person: int, start_person: int, pop_number: int):
+	if total_person <= 0 or start_person <= 0 or pop_number <= 0:
+		raise ValueError("parameters cannot be 0")
+	input = list(range(1, total_person + 1))
+	crt_idx= start_person - 1
+	for remain_person in range(total_person, 0, -1):
+		crt_idx = (crt_idx + pop_number - 1) % remain_person
+		print("josephus_delete_elem 0 pop item value is ", input.pop(crt_idx))
 
 if __name__ == '__main__':
 	josephus_delete_elem(4, 2, 5)
+	josephus_mark_elem(4, 2, 5)
