76c0e884c66e9fdb49a4f643a432ea159287f638
kwu <SERAPHWU@FOXMAIL.COM>
2019-05-09 21:21:00 +0800

    Refactor the function to make it simple.

--- Python/Data_Structure_and_Algorithm/josephus_question.py
+++ Python/Data_Structure_and_Algorithm/josephus_question.py
@@ -3,25 +3,24 @@
 # 直到所有人退出。
 # 结果：按照顺序输出每个退出人的编号。
 
-
-def josephus_mark_elem(total_person: int, start_person: int, pop_number: int):
-	if total_person <= 0 or start_person <= 0 or pop_number <= 0:
+def josephus_mark_elem(total_person: int, start_person: int, period: int):
+	if total_person <= 0 or start_person <= 0 or period <= 0:
 		raise ValueError("parameters cannot be 0")
 	crt_index = start_person - 1
-	input = list(range(1, total_person + 1))
-	for remain_item in range(total_person, 0, -1):
-		valid_count = 0
-		inc = 0
-		while valid_count < pop_number:
+	person_list = list(range(1, total_person + 1))
+	for remain_person in range(total_person, 0, - 1):
+		p = 0
+		while p < period:
+			if person_list[crt_index] > 0:
+				p += 1
+			if p == period:
+				print("mark person is ", person_list[crt_index])
+				person_list[crt_index] = 0
 			# 千万小心，这个方法只标记0，不delete，所以下面始终是 % total_person
-			crt_index = (crt_index + inc) % total_person
-			if input[crt_index] == 0:
-				pass
-			else:
-				valid_count += 1
-			inc = 1
-		print("josephus_mark_elem - pop item value is ", input[crt_index])
-		input[crt_index] = 0
+			# 最后一次while循环的时候，person_list[crt_index]已经被设置成0了，
+			# 所以设置下面的代码不会影响后续for循环里初始的状态
+			crt_index = (crt_index + 1) % total_person
+			
 	
 def josephus_delete_elem(total_person: int, start_person: int, pop_number: int):
 	if total_person <= 0 or start_person <= 0 or pop_number <= 0:
@@ -33,5 +32,6 @@ def josephus_delete_elem(total_person: int, start_person: int, pop_number: int):
 		print("josephus_delete_elem 0 pop item value is ", input.pop(crt_idx))
 
 if __name__ == '__main__':
-	josephus_delete_elem(4, 2, 5)
-	josephus_mark_elem(4, 2, 5)
+	josephus_delete_elem(7, 2, 30)
+	josephus_mark_elem(7, 2, 30)
+	
