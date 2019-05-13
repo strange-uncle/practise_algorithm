430394367efe6719940f149b2e131153c0f7a55d
kwu <SERAPHWU@FOXMAIL.COM>
2019-05-13 21:33:17 +0800

    Add a very basic str match algorithm.

---
+++ Python/Data_Structure_and_Algorithm/normal_str_match.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+
+# the very basic str match algorithm
+def str_match(t: str, p: str):
+	for i in range(len(t)):
+		idx = 0
+		while tar[i] == p[idx]:
+			if idx == len(p) - 1:
+				print("index in target str is ", str(i - idx))
+				return
+			i += 1
+			idx += 1
+	print("not found.")
+	
+if __name__ == "__main__":
+	tar = "0123456789"
+	p = "678"
+	str_match(tar, p)
+
+	p = "6780"
+	str_match(tar, p)
